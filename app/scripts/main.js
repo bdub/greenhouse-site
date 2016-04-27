0d97db8b0ae43ef64fb51072c867e6d8c327b293
Ben <ben@forward.net.au>
2016-04-27 16:23:10 +1000

    May / Meat vs Veg content added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+$('.toggle-button').click(function(){ //you can give id or class name here for $('button')
+    $(this).text(function(i, old){
+        return old === 'Show Details' ? 'Hide Details' : 'Show Details';
+    });
+});
