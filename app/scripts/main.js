e65e313ac1f501ea5a069f10001bc272d9d8e2d8
Ben <ben@forward.net.au>
2017-10-17 11:29:00 +1100

    july, august, sept 2017 updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,26 @@
 'use strict';
+
 $('.toggle-button').click(function(){ //you can give id or class name here for $('button')
     $(this).text(function(i, old){
         return old === 'Show Details' ? 'Hide Details' : 'Show Details';
     });
 });
+
+
+(function ($) {
+  $(document).ready(function(){
+	// hide .navbar first
+	$('.navbar').hide();
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 500) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+	});
+});
+  }(jQuery));
